2342fa5f29e7dbc931d104f473799b51f50c1627
Caiqueerodrigues <110926789+Caiqueerodrigues@users.noreply.github.com>
2022-10-20 23:16:16 -0300

    bugs

--- _script/sript.js
+++ _script/sript.js
@@ -4,22 +4,6 @@
         inicial.style.display = 'none'   
         inicial2.style.display = 'block'
     }
-    function fim () {
-        var final = document.getElementById('tela-final')
-        var fim = document.querySelector('h1#final')
-        /*daqui pra baixo é para adc a saudção*/
-        var agora = new Date()
-        var hr = agora.getHours()
-        final.style.display = 'none'
-        fim.style.display = 'block'
-        if (hr <12) {
-            fim.innerHTML += `<br>Bom Dia`
-        } else if (hr >=12 && hr <18) {
-            fim.innerHTML += `<br>Boa Tarde`
-        } else if (hr >= 18 && hr <= 23)
-            fim.innerHTML += `<br>Boa Noite`
-    }
-
 
 var visor1 = document.getElementById('pri')
     visor1.value = `0`
@@ -109,38 +93,55 @@ var presidente = [
             visor2.innerHTML = 0
             visor2.value = `0.0`
         }
-        var  voto = visor1.value + visor2.value
+        if (value == `bco`) {
+          visor1.value = `1.5`
+          visor2.value = `1.5`  
+        }
+var  voto = visor1.value + visor2.value
 
         switch (voto) {
             case `23`: {
                 presidente[1].innerHTML = `Luigi Brós`
-                presidente[2].innerHTML = `Partido Popular`
-                presidente[4].innerHTML = `Mariazinha da Padaria`
+                presidente[2].innerHTML = `Popular`
+                presidente[4].innerHTML = `Mariazinha`
             }
             break
             case `45`: {
                 presidente[1].innerHTML = `Mário Brós`
-                presidente[2].innerHTML = `Partido Trabalhista`
+                presidente[2].innerHTML = `Do Povo`
                 presidente[4].innerHTML = `Prof Augusto`
             }
             break
             case `33`: {
                 presidente[1].innerHTML = `Robô Léo`
-                presidente[2].innerHTML = `Partido Liberal`
+                presidente[2].innerHTML = `Liberal`
                 presidente[4].innerHTML = `João do Povo`
             }
             break
             case `16`: {
                 presidente[1].innerHTML = `Yoshi Munchakoopas`
-                presidente[2].innerHTML = `Partido Tecnológico`
+                presidente[2].innerHTML = `Tecnológico`
                 presidente[4].innerHTML = `Pedrinho Mecânico`
             }
+            break
+            case '1.51.5': {
+                presidente[0].innerHTML = `Branco`
+                presidente[1].innerHTML = `Branco`
+                presidente[2].innerHTML = `Branco`
+                presidente[3].innerHTML = `Branco`
+                presidente[4].innerHTML = `Branco`
+                visor1.value = 1.5
+                visor2.value = 1.5
+            }
+            break
             default: {
                 presidente[0].innerHTML = `Voto Nulo`
                 presidente[1].innerHTML = `Voto Nulo`
                 presidente[2].innerHTML = `Voto Nulo`
                 presidente[3].innerHTML = `Voto Nulo`
                 presidente[4].innerHTML = `Voto Nulo`
+                visor1.value = 1.5
+                visor2.value = 1.5
             }
         
     }
@@ -158,4 +159,25 @@ var presidente = [
         presidente[2].innerHTML = ` `
         presidente[3].innerHTML = ` `
         presidente[4].innerHTML = ` `
+    }
+
+    function fim () {
+            if (visor1.value != 0 && visor1.value != 0) {
+            var final = document.getElementById('tela-final')
+            var fim = document.querySelector('h1#final')
+            /*daqui pra baixo é para adc a saudção*/
+            var agora = new Date()
+            var hr = agora.getHours()
+            final.style.display = 'none'
+            fim.style.display = 'block'
+            }else {
+                alert(`[ERRO] Por favor, Escolha um Candidato`)
+            }
+            if (hr <12) {
+                fim.innerHTML += `<br>Bom Dia`
+            } else if (hr >=12 && hr <18) {
+                fim.innerHTML += `<br>Boa Tarde`
+            } else if (hr >= 18 && hr <= 23) {
+                fim.innerHTML += `<br>Boa Noite`
+            }
     }
